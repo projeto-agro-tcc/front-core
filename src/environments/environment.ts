e660f95d8190875cdd055cf026eec6249bc6ecac
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2021-12-10 08:30:32 -0300

    branch atualizada - 24-11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:8000/',
+  production: true,
+  // baseUrl: 'http://localhost:8000/',
+  baseUrl: 'http://monitoramento-dev.eba-sk3bsb2d.sa-east-1.elasticbeanstalk.com/',
   viacepURL: 'https://viacep.com.br/ws/',
   json: '/json'
 };
