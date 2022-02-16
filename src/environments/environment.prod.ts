295f823ab40679651ea2bdef6d38e5dba4f25b1a
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-16 20:43:10 -0300

    branch atualizada - 24-11

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  baseUrl: 'http://backend-core-env.eba-tm9q638y.sa-east-1.elasticbeanstalk.com/',
+  viacepURL: 'https://viacep.com.br/ws/',
+  json: '/json'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
-  production: true,
-  // baseUrl: 'http://localhost:8000/',
-  baseUrl: 'http://monitoramento-dev.eba-sk3bsb2d.sa-east-1.elasticbeanstalk.com/',
+  production: false,
+  baseUrl: 'http://localhost:8000/',
+  //baseUrl: 'http://monitoramento-dev.eba-sk3bsb2d.sa-east-1.elasticbeanstalk.com/',
   viacepURL: 'https://viacep.com.br/ws/',
   json: '/json'
 };
